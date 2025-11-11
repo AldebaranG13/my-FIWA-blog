bb00922884a828363d332d96d55c3de8b4160c3f
Aldebaran Gibran Altaf <aldebaran.gibran@fiwa.sch.id>
2025-11-11 07:16:44 +0000

    changed the preview text

--- scripts/posts-db.js
+++ scripts/posts-db.js
@@ -11,13 +11,13 @@ const allPosts = [
     title: "First 3D Modelling",
     url: "posts/first-3d-modelling.html",
     date: "November 9, 2025",
-    preview: "Have you ever got the time where your teacher suddenly assigns you a task...? Well that happened to me 2 weeks ago..."
+    preview: "Have you ever got the time where your teacher suddenly assigns you a task that you have never even learned about?"
   },
   {
     title: "My First Post",
     url: "posts/my-first-post.html",
     date: "November 8, 2025",
-    preview: "This is the first post for my blog. I'm building this site from scratch for my WICE competition..."
+    preview: "This is my first post for the website I am learning how to build a website from scratch using just HTML and CSS."
   }
   // == ADD NEW POSTS HERE (in the future) ==
   // {
